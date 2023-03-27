3c24d5a65bb9e6bf65528fd770bb4013f6d106b9
Kimin Lee <kiminlee2000225@gmail.com>
2023-03-27 14:40:49 -0700

    2023 updates

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -17,21 +17,18 @@ class About extends Component {
                   </ScrollAnimation>
                      <ScrollAnimation animateIn="fadeInLeft" animateOnce={true} delay={300} offset={250}>
                       <h2>Hi! I’m Kimin, a user-focused software engineer and a player-minded 
-                        game developer. I’m currently pursuing a Bachelor of Science degree in
-                         <a href="https://www.khoury.northeastern.edu/programs/bs-combined-major-in-computer-science-and-game-development/" className="highlightedText"> Computer Science & Game Development </a> 
-                        at Northeastern University. I’m 
+                        game developer. I’m currently a simulations software engineer at <a href="https://everydayrobots.com/" className="highlightedText"> Everyday Robots </a> at 
+                        <a href="https://x.company/" className="highlightedText"> X, the Moonshot Factory</a> (previously called Google X). I’m 
                         passionate about building innovative software and developing breath-taking 
                         and immersive games.</h2>
-                      <h2>Currently, I am a software engineer for cloud services co-op at 
-                        <a href="https://www.motorolasolutions.com/en_us.html" rel="noopener noreferrer" target="_blank" className="highlightedText"> Motorola Solutions</a>, working as a 
-                        full-stack developer.</h2>
-                      <h2>Alongside that, I am a Software Engineer/Developer at <a href="https://www.c4cneu.com" className="highlightedText"> Code 4 Community</a>, a Sponsorship member 
-                        of <a href="https://hackbeanpot.com" className="highlightedText"> HackBeanpot</a>, and a programmer/game developer for <a className="highlightedText"> Peach Beach Studios</a>.
+                      <h2>I have experiences working as a full-stack cloud services software engineer at
+                        <a href="https://www.motorolasolutions.com/en_us.html" rel="noopener noreferrer" target="_blank" className="highlightedText"> Motorola Solutions</a>, and a Software Engineer/Developer at <a href="https://www.c4cneu.com" className="highlightedText"> Code 4 Community</a>.</h2>
+                      <h2>Alongside that, I was a game programmer for <a href="https://www.ricegames.net/" className="highlightedText"> Rice Games</a>, working on <a href="https://www.shujinkou.net/" className="highlightedText"> Shujinkou</a>.
                       </h2>
                       <h2>When I’m not coding or building games, I’m either jamming to J-Pop/Korean R&B, learning new songs on the guitar,
                         immersing myself in the vast world of video games, or relaxing in Japanese-style 
                         hot springs. Some of my favorite games are Kid Icarus Uprising, League of Legends, Overwatch, FIFA, Overcooked, Disney Villainous, Secret Hitler, and of course, 
-                        the Super Smash Bros. series (Incineroar main). 
+                        the Super Smash Bros. series (Incineroar main). I've also recently been exploring VTubers!
                       </h2>
                       </ScrollAnimation>
                     </div>
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -22,13 +22,13 @@ class Contact extends Component {
                         <ScrollAnimation animateIn="fadeInDown" offset={-500} animateOnce={true} delay={400} >
                         <div className="searchTime">
                           <div className="resumeLink">
-                            <h2>Psst! I’m looking for opportunities in between <a href={null}>May 2021 - August 2021!</a></h2>
+                            <h2>Due to the recent layoffs at Google/Alphabet, I’m actively looking for opportunities!</h2>
                           </div>
                         </div>
                         </ScrollAnimation>
                         <ScrollAnimation animateIn="fadeInDown" offset={-500} animateOnce={true} delay={400} >
                           <div className="email">
-                            <h2>Check out my <a href="assets/files/Resume.pdf" target = "_blank">Resume!</a></h2>
+                            <h2>Check out my <a href="assets/files/Resume_2023_Kimin_Lee.pdf" target = "_blank">Resume!</a></h2>
                           </div>
                         </ScrollAnimation>
                         <ScrollAnimation animateIn="fadeInDown" offset={-500} animateOnce={true} delay={400} >
